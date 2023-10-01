fb296a3aca5071da6380f7cd425f35a63bdacef2
Eduard Tanase <eduard.tanase1@gmail.com>
2023-10-01 15:39:42 -0400

    File upload/download

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -13,12 +13,12 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({storage: storage});
 
 exports.uploadFiles = (req, res) => {
     upload.array('files', 10)(req, res, async (err) => {
         if (err) {
-            return res.status(400).send({ error: err.message });
+            return res.status(400).send({error: err.message});
         }
 
         try {
@@ -38,7 +38,60 @@ exports.uploadFiles = (req, res) => {
                 encryptedFiles
             });
         } catch (error) {
-            res.status(500).send({ error: 'Error encrypting files' });
+            res.status(500).send({error: 'Error encrypting files'});
         }
     });
 };
+
+// ...
+
+exports.downloadFile = (req, res) => {
+    const {filename} = req.params;
+
+    // Path to the encrypted file
+    const filePath = path.resolve('uploads', filename);
+
+    try {
+        if (!fs.existsSync(filePath)) {
+            return res.status(404).json({error: 'File not found'});
+        }
+
+        // Create read and write streams
+        const input = fs.createReadStream(filePath);
+        const decipher = crypto.createDecipher('aes-256-cbc', 'secretkey');
+
+        // Set the response headers
+        res.setHeader('Content-Disposition', 'attachment; filename=' + path.basename(filename.replace('.enc', '')));
+        res.setHeader('Content-Transfer-Encoding', 'binary');
+
+        // Decrypt the file on the fly and send it
+        input.pipe(decipher).pipe(res);
+    } catch (error) {
+        res.status(500).json({error: 'Error downloading the file'});
+    }
+};
+
+exports.listFiles = (req, res) => {
+    const directoryPath = path.join(__dirname, '..', 'uploads');
+
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+            return res.status(500).json({error: 'Unable to scan directory'});
+        }
+
+        const fileList = files.map(file => {
+            const filePath = path.join(directoryPath, file);
+            const stat = fs.statSync(filePath);
+
+            return {
+                name: file,
+                size: stat.size,
+                createdAt: stat.birthtime,
+                modifiedAt: stat.mtime
+            };
+        });
+
+        res.status(200).json(fileList);
+    });
+};
+
--- routes/files.js
+++ routes/files.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { uploadFiles } = require('../controllers/fileController');
+const { uploadFiles, downloadFile, listFiles } = require('../controllers/fileController');
 
 router.post('/upload', uploadFiles);
+router.get('/download/:filename', downloadFile);
+router.get('/', listFiles);
 
 module.exports = router;
