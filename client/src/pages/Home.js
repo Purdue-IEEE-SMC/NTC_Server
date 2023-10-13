76d5eea708644294d8e5113fd2338397f9a009e1
Eduard Tanase <eduard.tanase1@gmail.com>
2023-10-13 17:36:07 -0400

    Catppuccin Theme

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 function Home() {
   return (
     <div className="Home">
-      <h1>Home</h1>
+      <Container>
+        <h1>Home</h1>
+      </Container>
     </div>
   );
 }
