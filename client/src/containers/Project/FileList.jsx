ce8aa23ece659660f8982673e3110e01bd6ce87b
Eduard Tanase <eduard.tanase1@gmail.com>
2023-10-25 17:57:13 -0400

    feat: file deletion confirmation

--- client/src/containers/Project/FileList.jsx
+++ client/src/containers/Project/FileList.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Alert, Spinner, Table } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Alert, Button, Modal, Spinner, Table } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { Download, Trash } from 'react-bootstrap-icons';
 import { useDeleteFileMutation, useGetFilesQuery } from '../../services/files/filesApiSlice';
@@ -14,11 +14,23 @@ FileList.propTypes = {
 function FileList({ projectId, type }) {
   const { data: files, isLoading, isError, error } = useGetFilesQuery({ projectId, type });
   const [deleteFile] = useDeleteFileMutation();
+  const [show, setShow] = useState(false);
 
+  const handleDeleteFile = (filename) => {
+    setToDelete(filename);
+    setShow(true);
+  };
+
+  const handleConfirmDelete = async () => {
+    await deleteFile({ projectId, filename: toDelete });
+    setShow(false);
+    setToDelete(null);
+  };
+
+  const [toDelete, setToDelete] = useState(null);
   if (isLoading) {
     return <Spinner animation="border" role="status" />;
   }
-
   if (isError) {
     return <Alert variant="danger">Error: {error?.data?.message}</Alert>;
   }
@@ -26,6 +38,20 @@ function FileList({ projectId, type }) {
   if (files?.results && files.totalResults > 0) {
     return (
       <section className="files-list">
+        <Modal show={show} onHide={() => setShow(false)}>
+          <Modal.Body>
+            <p>Are you sure you would like to delete this file?</p>
+            <div className="d-flex justify-content-end">
+              <Button variant="secondary" className="mx-1" onClick={() => setShow(false)}>
+                Close
+              </Button>
+              <Button variant="danger" className="mx-1" onClick={handleConfirmDelete}>
+                Delete
+              </Button>
+            </div>
+          </Modal.Body>
+        </Modal>
+
         <Table>
           <thead>
             <tr>
@@ -48,7 +74,7 @@ function FileList({ projectId, type }) {
                   <button
                     className="mx-1 action-button delete-button"
                     type="submit"
-                    onClick={() => deleteFile({ projectId, filename: file.filename })}
+                    onClick={() => handleDeleteFile(file.filename)}
                   >
                     <Trash />
                   </button>
