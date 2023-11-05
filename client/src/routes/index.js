ae8a1b8a17ae329a30f76e918fb867a7342aadcf
Eduard Tanase <eduard.tanase1@gmail.com>
2023-11-05 15:16:49 -0500

    fix: vercel db stuff

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import RequireUnauth from '../services/auth/RequireUnauth';
-import RequireAuth from '../services/auth/RequireAuth';
 import RequireAdmin from '../services/auth/RequireAdmin';
 
 import Home from '../containers/Home/Home';
@@ -20,11 +19,9 @@ function AppRoutes() {
         <Route element={<RequireUnauth />}>
           <Route path="login" element={<Login />} />
         </Route>
-        <Route element={<RequireAuth />}>
-          <Route path="projects" element={<ProjectsPage />} />
-          <Route path="projects/:projectId" element={<Project />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
+        <Route path="projects" element={<ProjectsPage />} />
+        <Route path="projects/:projectId" element={<Project />} />
+        <Route path="*" element={<NotFound />} />
         <Route element={<RequireAdmin />}>
           <Route path="admin" element={<AdminPage />} />
         </Route>
