1a7d0deb14a27c9b097127f723f3d774e0554360
Eduard Tanase <eduard.tanase1@gmail.com>
2023-11-08 18:09:30 -0500

    fix: Test document timestamps

--- api/tests/fixtures/user.fixture.js
+++ api/tests/fixtures/user.fixture.js
@@ -6,6 +6,7 @@ const User = require('../../src/models/user.model');
 const password = 'password1';
 const salt = bcrypt.genSaltSync(8);
 const hashedPassword = bcrypt.hashSync(password, salt);
+const baseTimestamp = new Date();
 
 const userOne = {
   _id: mongoose.Types.ObjectId(),
@@ -35,7 +36,14 @@ const admin = {
 };
 
 const insertUsers = async (users) => {
-  await User.insertMany(users.map((user) => ({ ...user, password: hashedPassword })));
+  await User.insertMany(
+    users.map((user, index) => ({
+      ...user,
+      password: hashedPassword,
+      createdAt: new Date(baseTimestamp.getTime() + index),
+      updatedAt: new Date(baseTimestamp.getTime() + index),
+    }))
+  );
 };
 
 module.exports = {
