d1acbeb2fea5ab404191be1b91fae44ca00eb721
Eduard Tanase <eduard.tanase1@gmail.com>
2023-11-09 17:40:59 -0500

    fix: can't test env :(

--- api/tests/integration/docs.test.js
+++ api/tests/integration/docs.test.js
@@ -1,14 +1,11 @@
 const request = require('supertest');
 const httpStatus = require('http-status');
 const app = require('../../src/app');
-const config = require('../../src/config/config');
 
 describe('Auth routes', () => {
   describe('GET /api/v1/docs', () => {
-    test('should return 404 when running in production', async () => {
-      config.env = 'production';
+    test('should return 404 not running in development', async () => {
       await request(app).get('/api/v1/docs').send().expect(httpStatus.NOT_FOUND);
-      config.env = process.env.NODE_ENV;
     });
   });
 });
