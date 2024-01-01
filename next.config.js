2540de3d8e0bf9fcd90512135bde29b53074b449
Eduard Tanase <eduard.tanase1@gmail.com>
2024-01-01 15:05:35 -0700

    Add payload??

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+const { withPayload } = require('@payloadcms/next-payload');
+
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = withPayload(nextConfig, {
+  configPath: path.resolve(__dirname, './payload/payload.config.ts'),
+});
